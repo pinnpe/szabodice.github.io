677d2205fdb6ddcdd08d4d7674bb57f217dd6cad
szabodice <szabodice@gmail.com>
2016-03-04 18:53:59 -0600

    update

--- bundle.js
+++ bundle.js
@@ -68444,7 +68444,7 @@ var config = {};
 config.contract_dice = 'dice.sol';
 config.addrs = ["0x5f2bd66ac0653702530d3b899a96ad977eb0a0e1"];
 config.home_url = 'http://szabodice.github.io';
-config.home_url = 'http://localhost:8080';
+// config.home_url = 'http://localhost:8080';
 config.eth_testnet = true;
 
 try {
