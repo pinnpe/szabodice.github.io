7d8375e3e49d754f4bc98837eb6802950ea728f9
szabodice <szabodice@gmail.com>
2016-03-07 17:47:54 -0600

    update

--- bundle.js
+++ bundle.js
@@ -68442,9 +68442,9 @@ module.exports = {Main: Main, utility: utility};
 var config = {};
 
 config.contract_dice = 'dice.sol';
-config.addrs = ["0x5f2bd66ac0653702530d3b899a96ad977eb0a0e1"];
-config.home_url = 'http://szabodice.github.io';
-// config.home_url = 'http://localhost:8080';
+config.addrs = ["0x7353de0d4d9f86fb80a94a4f99f5e13c9c980a90"];
+// config.home_url = 'http://szabodice.github.io';
+config.home_url = 'http://localhost:8080';
 config.eth_testnet = true;
 
 try {
--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 var config = {};
 
 config.contract_dice = 'dice.sol';
-config.addrs = ["0x5f2bd66ac0653702530d3b899a96ad977eb0a0e1"];
-config.home_url = 'http://szabodice.github.io';
-// config.home_url = 'http://localhost:8080';
+config.addrs = ["0x563dfdf93e7d7f8bb226319d1fd96a0f78a00c78"];
+// config.home_url = 'http://szabodice.github.io';
+config.home_url = 'http://localhost:8080';
 config.eth_testnet = true;
 
 try {
